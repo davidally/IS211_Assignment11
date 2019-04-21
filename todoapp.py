081fdd4ede12de0ba6de2c9eeec404d23ab9659b
david <davidally94@gmail.com>
2019-04-20 22:11:06 -0400

    Fixed typo

--- todoapp.py
+++ todoapp.py
@@ -15,7 +15,15 @@ def submission():
     email = request.form['email']
     priority = request.form['priority']
     task_list.append(task)
+    print task_list
     return redirect('/')
+
+
+@app.route('/clear', methods=['POST'])
+def clear_list():
+    task_list = []
+    print task_list
+    return render_template('index.html', task_list=task_list)
 
 
 if __name__ == '__main__':
